7a3765b0456283a822f2f63b0286ffbe6bd1c843
Romesh Kumbhani <kumbhani@gmail.com>
2016-03-25 15:34:19 -0400

    my turn is over

--- BestTimetoBuy.py
+++ BestTimetoBuy.py
@@ -39,12 +39,21 @@ class Solution(object):
        #      for j,second_price in enumerate(prices[i+1:]):
        #          best_increase = max(second_price - first_price, best_increase)
 
+       # Find Global Minima and Maxima 
+       (max_val,min_val) = self.find_extremes(prices)
 
-
-        
-       return best_increase if best_increase > 0 else 0
+       # if the global maxima occurs after the global minima, we're done.
+       if min_val['location'] < max_val['location']:
+            return max_val['value'] - min_val['value']
+       else:
+            (A_max_val,A_min_val) = self.find_extremes(prices[:max_val['location']+1])
+            (B_max_val,B_min_val) = self.find_extremes(prices[min_val['location']:])
+            A_value = A_max_val['value'] - A_min_val['value']
+            B_value = B_max_val['value'] - B_min_val['value']
+            return A_value if A_value>B_value else B_value
 
    def find_extremes(self, prices):
+   #def find_extremes(prices):
       """
       Pass in a slice (or all) of prices and find the extreme max and min as well as their locations
 
@@ -55,13 +64,13 @@ class Solution(object):
       max_val, min_val - Dicts with keys value and location
 
 
+      *** What's the self for?, is it needed?
+
 
       """
 
-      max_val = {value = -999,
-             location = 0}
-      min_val = {value = +999,
-             location = 0}
+      max_val = {"value" : -999999999, "location" : 0}
+      min_val = {"value" : +999999999, "location" : 0}
 
       for i,price in enumerate(prices):
             if price > max_val['value']:
